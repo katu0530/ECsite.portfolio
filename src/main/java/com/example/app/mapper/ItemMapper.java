a76c2ebd9e940d76a862ae99ea530e57ae95cec8
sawada <123456@abc.com>
2024-01-22 11:55:48 +0900

    アイテムmapperファイル内のクラス名修正

--- src/main/java/com/example/app/mapper/ItemMapper.java
+++ src/main/java/com/example/app/mapper/ItemMapper.java
@@ -3,12 +3,11 @@ package com.example.app.mapper;
 import java.util.List;
 
 import com.example.app.domain.Item;
-import com.example.app.domain.Member;
 
 public interface ItemMapper {
 	
 	List<Item> selectAll() throws Exception;
-	Member selectById(Integer id) throws Exception;
+	Item selectById(Integer id) throws Exception;
 	void insert(Item item) throws Exception;
 	void update(Item item) throws Exception;
 	void delete(Integer id) throws Exception;
