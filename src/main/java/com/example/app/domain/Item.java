40d61a47e531cb7783dd09deaed1427159d09486
sawada <123456@abc.com>
2024-01-23 14:14:58 +0900

    エラーの修正
    データベース内のカラム名とごっちゃになってエラーがでていた。
    変数名はcamel caseが好ましいので、修正。

--- src/main/java/com/example/app/domain/Item.java
+++ src/main/java/com/example/app/domain/Item.java
@@ -1,6 +1,7 @@
 package com.example.app.domain;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -11,12 +12,12 @@ public class Item {
 	
 	@NotBlank
 	@Size(max=10)
-	private String item_name;
+	private String itemName;
 	
-	@NotBlank
+	@NotNull
 	private Integer price;
 	
-	private String item_introduction;
+	private String itemIntroduction;
 	
 
 }
