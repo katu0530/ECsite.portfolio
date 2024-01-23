d2cb8ece499440e27ab5204d993e660284650d66
sawada <123456@abc.com>
2024-01-23 16:10:07 +0900

    認証用フィルターの作成。

--- src/main/java/com/example/app/config/ApplicationConfig.java
+++ src/main/java/com/example/app/config/ApplicationConfig.java
@@ -1,11 +1,14 @@
 package com.example.app.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.app.filter.AuthFilter;
 
 @Configuration
 public class ApplicationConfig implements WebMvcConfigurer {
@@ -22,6 +25,15 @@ public class ApplicationConfig implements WebMvcConfigurer {
 	var messageSource = new ResourceBundleMessageSource();
 	messageSource.setBasename("validation");
 	return messageSource;
+	}
+	
+	// 認証用フィルタの有効化
+	@Bean
+	FilterRegistrationBean<AuthFilter> authFilter() {
+	var bean =
+	new FilterRegistrationBean<AuthFilter>(new AuthFilter());
+	bean.addUrlPatterns("/admintop/*");
+	return bean;
 	}
 
 }
