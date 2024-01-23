457c846241b9c214044e03a399820ed268288f7a
sawada <123456@abc.com>
2024-01-23 14:16:04 +0900

    商品一覧の削除・追加・編集機能の追加

--- src/main/java/com/example/app/controller/ItemController.java
+++ src/main/java/com/example/app/controller/ItemController.java
@@ -27,4 +27,56 @@ public class ItemController {
 		model.addAttribute("items", service.getItemList());
 		return "admin/itemlist";
 	}
+
+	@GetMapping("/add")
+	public String addGet(Model model) {
+		model.addAttribute("title", "商品の追加");
+		model.addAttribute("item", new Item());
+		return "admin/itemadd";
+	}
+
+	@PostMapping("/add")
+	public String addPost(@Valid Item item,
+			Errors errors,
+			RedirectAttributes rd,
+			Model model) throws Exception {
+		if (errors.hasErrors()) {
+			model.addAttribute("title", "商品の追加");
+			return "admin/itemadd";
+		}
+		service.addItem(item);
+		rd.addFlashAttribute("statusMessage", "会員を追加しました。");
+		return "redirect:/admin/itemlist";
+
+	}
+
+	@GetMapping("/edit/{id}")
+	public String editGet(@PathVariable Integer id, Model model) throws Exception {
+		model.addAttribute("title", "商品情報の変更");
+		model.addAttribute("item", service.getItemById(id));
+		return "admin/itemadd";
+	}
+
+	@PostMapping("/edit/{id}")
+	public String editPost(@PathVariable Integer id,
+			@Valid Item item,
+			Errors errors,
+			RedirectAttributes rd,
+			Model model) throws Exception {
+		if (errors.hasErrors()) {
+			model.addAttribute("title", "商品情報の変更");
+			return "admin/itemadd";
+		}
+		item.setId(id);
+		service.editItem(item);
+		rd.addFlashAttribute("statusMessage", "会員情報を更新しました。");
+		return "redirect:/admin/itemlist";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id, RedirectAttributes rd) throws Exception {
+		service.deleteItem(id);
+		rd.addFlashAttribute("statusMessage", "会員情報を削除しました。");
+		return "redirect:/admin/itemlist";
+	}
 }
