f256083357b58664a6a43630ff8e3e44f178e197
sawada <123456@abc.com>
2024-01-23 14:10:32 +0900

    エラーの修正
    @RequestMapping("/admin")を記載してマッピングの修正。

--- src/main/java/com/example/app/controller/ItemController.java
+++ src/main/java/com/example/app/controller/ItemController.java
@@ -2,24 +2,27 @@ package com.example.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.app.domain.Item;
 import com.example.app.service.ItemService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequestMapping("/admin")
 @RequiredArgsConstructor
 public class ItemController {
-	
+
 	private final ItemService service;
-	
-	@GetMapping("/admin")
-	public String adminTop(Model model) {
-		return "admin/adminTop";
-	}
-	
-	@GetMapping("/admin/itemlist")
+
+	@GetMapping("/itemlist")
 	public String list(Model model) throws Exception {
 		model.addAttribute("items", service.getItemList());
 		return "admin/itemlist";
