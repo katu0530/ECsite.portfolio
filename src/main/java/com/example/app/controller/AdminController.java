332b9fae485182c38cc373fb55ae5a0d74511d20
sawada <123456@abc.com>
2024-01-23 16:06:28 +0900

    IDとパスワードを確認するcontrollerの作成。

---
+++ src/main/java/com/example/app/controller/AdminController.java
@@ -0,0 +1,54 @@
+package com.example.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.app.domain.Admin;
+import com.example.app.service.AdminService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+	
+	private final AdminService service;
+	
+	@GetMapping
+	public String ShowHome(Model model) {
+		model.addAttribute("admin", new Admin());
+		return "admin/admintop";
+	}
+	
+	@PostMapping
+	public String login(@Valid Admin admin,
+			Errors errors,
+			HttpSession session) throws Exception {
+		if(errors.hasErrors()) {
+			return "admin/admintop";
+		}
+		
+		if(!service.isCorrectIdAndPassword(admin.getLoginId(), admin.getLoginPass())) {
+			errors.rejectValue("loginId", "error.incorrect_id_password");
+			return "admin/admintop";
+		}
+		
+		session.setAttribute("loginId", admin.getLoginId());
+		return "redirect:/admin";
+	}
+	
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/admin";
+	}
+
+}
